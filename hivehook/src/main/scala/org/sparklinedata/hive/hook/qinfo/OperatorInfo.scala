f2e819a0d46e6539c098628d2544c261c9badfd0
Harish Butani <hbutani@hortonworks.com>
2015-03-09 14:16:25 -0700

    extract details for Join, Select and GBys

--- hivehook/src/main/scala/org/sparklinedata/hive/hook/qinfo/OperatorInfo.scala
+++ hivehook/src/main/scala/org/sparklinedata/hive/hook/qinfo/OperatorInfo.scala
@@ -2,8 +2,9 @@ package org.sparklinedata.hive.hook.qinfo
 
 import java.io.Writer
 
-import org.apache.hadoop.hive.ql.exec.{Operator, TableScanOperator}
-import org.apache.hadoop.hive.ql.plan.TableScanDesc
+import org.apache.hadoop.hive.ql.exec.{Operator, TableScanOperator,
+FilterOperator, CommonJoinOperator, MapJoinOperator, SelectOperator, GroupByOperator}
+import org.apache.hadoop.hive.ql.plan.{TableScanDesc, FilterDesc, JoinDesc, JoinCondDesc, SelectDesc, GroupByDesc}
 import org.sparklinedata.hive.hook.PrintableNode
 import org.sparklinedata.hive.metadata.{PartitionDef, TableDef}
 
@@ -44,6 +45,12 @@ class OperatorInfo(val qInfo : QueryInfo, tInfo: TaskInfo, val op : Operator[_])
 object OperatorInfo {
   def apply(qInfo : QueryInfo, tInfo: TaskInfo, op : Operator[_]) : OperatorInfo = op match {
     case ts : TableScanOperator => new TableScanOperatorInfo(qInfo, tInfo, ts)
+    case filter : FilterOperator => new FilterOperatorInfo(qInfo, tInfo, filter)
+    case mjoin : MapJoinOperator => new MapJoinOperatorInfo(qInfo, tInfo, mjoin)
+    case join : CommonJoinOperator[_] =>
+      new JoinOperatorInfo(qInfo, tInfo, join.asInstanceOf[CommonJoinOperator[_ <: JoinDesc]])
+    case sel : SelectOperator => new SelectOperatorInfo(qInfo, tInfo, sel)
+    case gBy : GroupByOperator => new GroupByOperatorInfo(qInfo, tInfo, gBy)
     case _ => new OperatorInfo(qInfo, tInfo, op)
   }
 }
@@ -93,5 +100,123 @@ class TableScanOperatorInfo(qInfo : QueryInfo, tInfo: TaskInfo, op : TableScanOp
   override def operatorDetails(prefix : String) : String = {
     val p = prefix + "    "
     s"(${tempFileStr(p)}${tableStr(p)}${parttionStr(p)}${aliasStr(p)}${projectColumnsStr(p)}${filterExprStr(p)}\n$p)"
+  }
+}
+
+class FilterOperatorInfo(qInfo : QueryInfo, tInfo : TaskInfo, op : FilterOperator)
+  extends OperatorInfo(qInfo, tInfo, op) {
+
+  def conf : FilterDesc = op.getConf
+  def predicate = conf.getPredicate
+
+  def predicateStr = predicate.getExprString
+
+  override def operatorDetails(prefix : String) : String = {
+    s"(predicate=${predicateStr})"
+  }
+}
+
+class JoinOperatorInfo(qInfo : QueryInfo, tInfo : TaskInfo, op : CommonJoinOperator[_ <: JoinDesc])
+  extends OperatorInfo(qInfo, tInfo, op) {
+
+  def conf = op.getConf
+  def joinConds = conf.getKeysString
+  def filters = conf.getFilters
+  def nullSafes = conf.getNullSafes
+  def joinCondTypes = conf.getConds
+
+  def aliases : IndexedSeq[String] = (conf.getLeftAlias :: conf.getRightAliases.toList).toIndexedSeq
+
+  def joinStr(condDesc : JoinCondDesc, nullSafe : Boolean) : String = {
+
+    val leftAlias = condDesc.getLeft.toByte
+    val rightAlias = condDesc.getRight.toByte
+
+    val joinType = condDesc.getType match {
+      case 0 =>  "Inner Join "
+      case 1 => "Left Outer Join"
+      case 2 => "Right Outer Join"
+      case 3 => "Outer Join "
+      case 4 => "Unique Join"
+      case 5 => "Left Semi Join "
+      case _ => "Unknow Join "
+    }
+
+    def eqOp = if (nullSafe) "<=>" else "="
+
+    val lExprs = joinConds(leftAlias)
+    val rExprs = joinConds(rightAlias)
+
+    val joinPredicates = s"leftCond = $lExprs, rightCond = $rExprs"
+
+    val lFilters = filters(leftAlias).map{ _.getExprString}
+    val rFilters = filters(rightAlias).map{ _.getExprString}
+
+    val joinStr = {
+      var jStr = joinPredicates
+      if (lFilters.size > 0 ) {
+        jStr = jStr + (s", leftFilters = $lFilters")
+      }
+      if (rFilters.size > 0 ) {
+        jStr = jStr + (s", rightFilters = $rFilters")
+      }
+
+      s"($jStr)"
+    }
+
+    val leftAliasStr = aliases(leftAlias)
+    val rightAliasStr = aliases(rightAlias)
+
+    s"$leftAliasStr $joinType $rightAliasStr on $joinStr"
+  }
+
+  lazy val joinStr : String = {
+    (joinCondTypes zip nullSafes).map{t =>
+      val c = t._1
+      val n = t._2
+      joinStr(c, n)
+    }.mkString("\n")
+  }
+
+  override def operatorDetails(prefix : String) : String = {
+    _throwNPE
+    s"($joinStr)"
+  }
+
+}
+
+class MapJoinOperatorInfo(qInfo : QueryInfo, tInfo : TaskInfo, op : MapJoinOperator)
+  extends JoinOperatorInfo(qInfo, tInfo, op) {
+
+  override  def aliases : IndexedSeq[String] = IndexedSeq("Input_0", "Input_1")
+}
+
+class SelectOperatorInfo(qInfo : QueryInfo, tInfo : TaskInfo, op : SelectOperator)
+  extends OperatorInfo(qInfo, tInfo, op) {
+
+  def conf : SelectDesc = op.getConf
+
+  override def operatorDetails(prefix : String) : String = {
+    val selList = (conf.getColList zip conf.getOutputColumnNames).map { t =>
+      val e = t._1.getExprString
+      val al = t._2
+      s"$e as $al"
+    }.mkString(", ")
+    val p = prefix + "    "
+    s"(\n${p}selectList=${selList}\n$p)"
+  }
+}
+
+class GroupByOperatorInfo(qInfo : QueryInfo, tInfo : TaskInfo, op : GroupByOperator)
+  extends OperatorInfo(qInfo, tInfo, op) {
+
+  def conf : GroupByDesc = op.getConf
+
+  override def operatorDetails(prefix : String) : String = {
+    val keysStr = conf.getKeys.map(_.getExprString).mkString(", ")
+    val p = prefix + "    "
+    val aggStr = conf.getAggregators.map(_.getExprString).mkString("\n" + p + "  ")
+
+    s"(\n${p}keys=${keysStr}\n${p}aggregations=${aggStr}\n$p)"
   }
 }
